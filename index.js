ec2b87e5ed3c627ad5ce9360540950109eac6e3d
piyushbhawsar <111758968+piyushbhawsar@users.noreply.github.com>
2023-04-26 00:48:54 +0530

    first_base/multer

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const path = require("path")
+const multer  = require('multer')
+
+const app = express()
+const port = 3000 
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        return cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+        return cb(null , `${Date.now()}-${file.originalname}`)
+    }
+  })
+
+const upload = multer({ storage })
+
+app.set("view engine" , "ejs")
+app.set("views" , path.resolve('./views'))
+
+app.use(express.urlencoded({ extended:false }))
+
+app.get("/", (req,res) => {
+    return res.render('homepage')
+})
+app.post("/uploads", upload.single("profileImage"), (req,res)=> {
+    console.log(req.body)
+    console.log(req.file)
+    return res.redirect("/")
+})
+
+app.listen(port,() => console.log(`server started at ${port}`))
